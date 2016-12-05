231691fab2cee85b3e967fb0e5a31e5aa6733b0a
jenniferjara <lisbet.jara@gmail.com>
2016-12-05 17:27:48 -0500

    funciones de form

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,38 @@
 $(document).ready(function(){
 	$(".dropdown-button").dropdown();
+
+	/****form****/
+	$('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15 // Creates a dropdown of 15 years to control year
+    });
+    
+	$("form[name='myform']").validate({
+      // Specify validation rules
+      rules: {
+        lugar: "required",
+        fecha: "required",
+      },
+
+      messages: {
+        lugar: "Ingrese un lugar",
+        fecha: "Eliga una fecha",
+      },
+
+      submitHandler: function(form) {
+          console.log(form);
+          form.submit();
+      },
+      errorElement : 'div',
+      errorPlacement: function(error, element) {
+        console.log(element);
+        var placement = $(element).data('error');
+        if (placement) {
+          $(placement).append(error)
+        } else {
+          error.insertAfter(element);
+        }
+      }
+    });
+    /****form****/
 });
