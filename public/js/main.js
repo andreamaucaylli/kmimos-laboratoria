ec0e09d522ba798e72c41fd3a22df4f101aea745
jenniferjara <lisbet.jara@gmail.com>
2016-12-06 17:11:04 -0500

    vista de mapa

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-var template ='<div class="col s12 m4">' +
+var template ='<div class="col s12 m12">' +
                 '<div class="card horizontal hoverable">' +
                     '<div class="card-stacked">' +
                         '<div class="card-content teal lighten-2 white-text">' +
@@ -11,69 +11,97 @@ var template ='<div class="col s12 m4">' +
                         '</div>' +
                      '</div>' +
                 '</div>' +
-            '</div>'; 
-     
+            '</div>';
 
-$(document).ready(function(){
-  $(".dropdown-button").dropdown();
+    var map;
+    var infowindow;
+    var largo;
 
-  $(".datepicker").pickadate({
-      selectMonths: true,
-      selectYears: 15
-  });
-  var formu = $("#myform");
+    function initMap() {
+      var pyrmont = {lat: -33.867, lng: 151.195};
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: pyrmont,
+        zoom: 15
+      });
 
-  formu.validate({
-    rules: {
-      lugar: "required",
-      fecha: "required",
-    },
+      infowindow = new google.maps.InfoWindow();
 
-    messages: {
-      lugar: "Ingrese un lugar",
-      fecha: "Eliga una fecha",
-    },
+      var service = new google.maps.places.PlacesService(map);
+      service.nearbySearch({
+        location: pyrmont,
+        radius: 30
+      }, callback);
+    }
 
-    errorElement : 'div',
-    errorPlacement: function(error, element) {
-      var placement = $(element).data("error");
-        if (placement) {
-          $(placement).append(error)
-        } else {
-        error.insertAfter(element);
+    function callback(results, status, largo) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < largo; i++) {
+          console.log(largo);
+          createMarker(largo[i]);
         }
       }
-    });
+    }
 
-  $("#btn").click(function(e){
-    e.preventDefault();
+    function createMarker(place) {
+      var placeLoc = place.geometry.location;
+      var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+      });
 
-    formu.validate();
-    var maxImages = 9;
-    var distritoIngresado = $('#icon_prefix').val();
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+      });
+    }
+            
+$(document).ready(function(){
 
-    $.when(
-      $.ajax({ 
-        url: "http://localhost:3000/cuidadores?lugar="+distritoIngresado,
-        type: "GET"
-      }), 
-      $.ajax({
-        url: 'https://randomuser.me/api?inc=picture&results=' + maxImages,
-        dataType: 'json',
-        type: "GET"
-      })
-    ).then(function(cuidadores,data){
-      console.log(cuidadores);
-      console.log(data);
-      $.each(cuidadores[0], function (i,cuidador) {
-        var cuidadorResultado = template.replace("{{name}}", cuidador.name)
-          .replace("{{age}}", cuidador.age)
-          .replace("{{address}}", cuidador.address)
-          .replace("{{distrito}}", cuidador.distrito)
-          .replace("{{phone}}", cuidador.contact.phone)
-          .replace("{{image}}", data[0].results[i % maxImages].picture.medium);
-          $("#resultados").append(cuidadorResultado);
+  $("#map").hide();
+  $("#icon_prefix").keyup(validarForm);
+    function validarForm () {
+      var distritoInput = $(this).val();
+      var valor = true;
+      var regexDistrito = /^[a-zñáéíóúü]+$/gi;
+        if (!regexDistrito.test(distritoInput)) {
+          valor = false;
+        }
+    }
+
+    var sigt = function(e){
+      e.preventDefault();
+      initMap();
+      var maxImages = 9;
+      var distritoIngresado = $('#icon_prefix').val();  
+      $.when(
+        $.ajax({ 
+          url: "/cuidadores?lugar="+distritoIngresado,
+          type: "GET"
+        }), 
+        $.ajax({
+          url: 'https://randomuser.me/api?inc=picture&results=' + maxImages,
+          dataType: 'json',
+          type: "GET"
+        })
+      ).then(function(cuidadores,data){
+        $.each(cuidadores[0], function (i,cuidador) {
+          largo = cuidadores[0].length;
+          console.log(largo);
+          initMap();
+          var cuidadorResultado = template.replace("{{name}}", cuidador.name)
+            .replace("{{age}}", cuidador.age)
+            .replace("{{address}}", cuidador.address)
+            .replace("{{distrito}}", cuidador.distrito)
+            .replace("{{phone}}", cuidador.contact.phone)
+            .replace("{{image}}", data[0].results[i % maxImages].picture.medium);
+            $("#resultados").append(cuidadorResultado);
         });
-    });    
-  });
+      });
+
+      $("#map").show();
+      $('#icon_prefix').val('');
+
+    };
+
+    $("#btn").click(sigt);
 });
--- server.js
+++ server.js
@@ -1,33 +1,25 @@
 var express = require("express");
 var app = express();
 var dream = require('dreamjs');
-
-
 dream.customType('pi', function () {
   return Math.PI;
 });
-
  var distritos = ['Lima', 'Ate', 'Barranco', 'Breña', 'Comas', 'Chorrillos', 'El Agustino', 'Jesús María', 
   'La Molina', 'La Victoria', 'Lince', 'Magdalena del Mar', 'Miraflores', 'Pueblo Libre', 'Puente Piedra',
   'Rimac', 'San Isidro', 'Independencia', 'San Juan de Miraflores', 'San Luis', 'San Martin de Porres', 'San Miguel',
   'Santiago de Surco', 'Surquillo', 'Villa María del Triunfo', 'San Juan de Lurigancho', 'Santa Rosa', 'Los Olivos',
   'San Borja', 'Villa El Savador', 'Santa Anita', 'Callao', 'Bellavista', 'Carmen de la Legua', 'La Perla', 'La Punta', 'Ventanilla'];
-
 dream.customType('distrito', function (helper) {
   return helper.oneOf(distritos);
 });
-
 var genero = ['women', 'men'];
-
 var numero = [];
 for (var i = 0; i <= 99; i++) {
     numero.push(i);
 }
-
 dream.customType('imagen', function(helper){
   return 'https://randomuser.me/api/portraits/med/' + helper.oneOf(genero) + '/' + helper.oneOf(numero) + '.jpg' ;
 });
-
 var cuidadores = dream
   .schema({
     name: 'name',
@@ -45,22 +37,20 @@ var cuidadores = dream
     pi: 'pi',
     hello: 'hello'
   })
-
   .generateRnd(100)
   .output();
   
 app.get('/cuidadores', function (req, res) {
-	var lugar = req.query.lugar;
-	var cuidadoresFiltrados = [];
-	for (var i = 0; i < cuidadores.length; i++) {
-		var cuidador = cuidadores[i];
-		if (cuidador.distrito == lugar) {
-			cuidadoresFiltrados.push(cuidador);
-		}		
-	}
-  	res.send(cuidadoresFiltrados);
+    var lugar = req.query.lugar;
+    var cuidadoresFiltrados = [];
+    for (var i = 0; i < cuidadores.length; i++) {
+        var cuidador = cuidadores[i];
+        if (cuidador.distrito == lugar) {
+            cuidadoresFiltrados.push(cuidador);
+        }       
+    }
+    res.send(cuidadoresFiltrados);
 })
-
 app.set("port", process.env.PORT || 3000);
 app.use(express.static(__dirname + "/public"));
 app.listen(app.get("port"), function(){
