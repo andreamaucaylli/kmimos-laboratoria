a33c7554ebf0486032483c2885623917173c4242
Andrea Maucaylli <andreamaucaylli@gmail.com>
2016-12-06 11:47:50 -0500

    Readme

--- public/js/main.js
+++ public/js/main.js
@@ -33,7 +33,7 @@ $(document).ready(function(){
 
     $.when(
       $.ajax({ 
-        url: "http://localhost:3000/cuidadores?lugar="+distritoIngresado,
+        url: "https://localhost:3000/cuidadores?lugar="+distritoIngresado,
         type: "GET"
       }), 
       $.ajax({
