3daf2201f79efacb10e617aa9e3d161bd8c787b9
Andrea Maucaylli <andreamaucaylli@gmail.com>
2016-12-06 12:00:15 -0500

    mvp1 https

--- public/js/main.js
+++ public/js/main.js
@@ -32,7 +32,7 @@ $(document).ready(function(){
 
     $.when(
       $.ajax({ 
-        url: "https://localhost:3000/cuidadores?lugar="+distritoIngresado,
+        url: "/cuidadores?lugar="+distritoIngresado,
         type: "GET"
       }), 
       $.ajax({
