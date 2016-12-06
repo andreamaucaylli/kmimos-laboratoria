83b04f5a10912a49ea7d8a832c97dfb98d4154e0
jenniferjara <lisbet.jara@gmail.com>
2016-12-05 19:28:10 -0500

    cambios de login

--- public/js/main.js
+++ public/js/main.js
@@ -3,11 +3,11 @@ $(document).ready(function(){
 
 	/****form****/
 	$('.datepicker').pickadate({
-        selectMonths: true, // Creates a dropdown to control month
-        selectYears: 15 // Creates a dropdown of 15 years to control year
-    });
-    
-	$("form[name='myform']").validate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
+  var formu = $("#myform");
+	formu.validate({
       // Specify validation rules
       rules: {
         lugar: "required",
@@ -19,19 +19,19 @@ $(document).ready(function(){
         fecha: "Eliga una fecha",
       },
 
-      submitHandler: function(form) {
-          console.log(form);
-          form.submit();
-      },
       errorElement : 'div',
       errorPlacement: function(error, element) {
-        var placement = $(element).data('error');
+        var placement = $(element).data("error");
         if (placement) {
           $(placement).append(error)
         } else {
           error.insertAfter(element);
         }
       }
-    });
+  });
+  $("#btn").click(function(){
+      formu.validate();
+  });
     /****form****/
+
 });
